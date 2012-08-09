0f61afa8afdcf7057a393234fc2827d2082e8fdf
Nick Babcock <nbabcock19@hotmail.com>
2012-08-09 07:50:35 -0400

    Initial documentation for IParadoxFile

--- Pdoxcl2Sharp/IParadoxFile.cs
+++ Pdoxcl2Sharp/IParadoxFile.cs
@@ -5,8 +5,17 @@ using System.Text;
 
 namespace Pdoxcl2Sharp
 {
+    /// <summary>
+    /// Defines a common interface for parsing a Paradox File
+    /// </summary>
     public interface IParadoxFile
     {
+        /// <summary>
+        /// This function is invoked as a callback when the parser encounters
+        /// a token and does not know how to handle it.
+        /// </summary>
+        /// <param name="parser">The parser that invoked the callback</param>
+        /// <param name="token">The token that the parser didn't know how to handle</param>
         void TokenCallback(ParadoxParser parser, string token);
     }
 }
