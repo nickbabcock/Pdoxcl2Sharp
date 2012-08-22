94132c6278ab3c69218b30b6728c5ce8b14e60ad
Nick Babcock <nbabcock19@hotmail.com>
2012-08-22 19:54:58 -0400

    Add multiple double list tests

--- Pdoxcl2Sharp.Test/MultipleListRead.cs
+++ Pdoxcl2Sharp.Test/MultipleListRead.cs
@@ -23,6 +23,20 @@ namespace Pdoxcl2Sharp.Test
             Test<int>(data, x => x.ReadIntList(), new[] { 1, 2, 3, 4 }, "list", "list2");
         }
 
+        [Test]
+        public void SimpleDoubleMultiple()
+        {
+            var data = "list = { 0.091815 0.000000 0.908185 } \r\n list2 = { 0.091815 0.000000 0.908185 }".ToByteArray();
+            Test<double>(data, x => x.ReadDoubleList(), new[] {0.091815, 0, 0.908185}, "list", "list2"); 
+        }
+
+        [Test]
+        public void NoSpaceDoubleMultiple()
+        {
+            var data = "list={0.091815 0.000000 0.908185} list2={0.091815 0.000000 0.908185}".ToByteArray();
+            Test<double>(data, x => x.ReadDoubleList(), new[] { 0.091815, 0, 0.908185 }, "list", "list2"); 
+        }
+
         private void Test<T>(byte[] data,
                              Func<ParadoxParser, IEnumerable<T>> func,
                              IEnumerable<T> expected,
