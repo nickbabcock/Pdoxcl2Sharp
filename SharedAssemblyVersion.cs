a1d3f10bc61f1b2e6f3f2b9a7eac1ef2d615cb53
Nick Babcock <nbabcock19@hotmail.com>
2012-08-23 22:26:53 -0400

    Solution shares a common assembly versioning

---
+++ SharedAssemblyVersion.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Pdoxcl2Sharp")]
+[assembly: AssemblyDescription("A Paradox Interactive general file parser.")]
+[assembly: AssemblyCompany("NBSoftSolutions")]
+[assembly: AssemblyProduct("Pdoxcl2Sharp")]
+[assembly: AssemblyCopyright("Copyright (C) Nick Babcock")]
+[assembly: AssemblyVersion("0.2.0.0")]
