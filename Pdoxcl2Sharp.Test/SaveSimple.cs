79eb4e3472b3b783aab78c8f8d7ba89ec22ffe41
Nick Babcock <nbabcock19@hotmail.com>
2012-12-20 15:04:24 -0500

    Rudimentary saving implemented

---
+++ Pdoxcl2Sharp.Test/SaveSimple.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Pdoxcl2Sharp;
+using System.IO;
+
+namespace Pdoxcl2Sharp.Test
+{
+    [TestFixture]
+    class SaveSimple
+    {
+        [Test]
+        public void SingleSave()
+        {
+            string input = "culture=michigan";
+            string newCulture = "ohio";
+            StringWriter save = new StringWriter();
+            
+            Action<ParadoxSaver, string> action = (p, s) =>
+                {
+                    if (s == "culture")
+                        p.WriteValue(newCulture);
+                };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            Assert.AreEqual("culture=ohio", save.ToString());
+        }
+
+        [Test]
+        public void SingleQuoteSave()
+        {
+            string input = "culture=\"michigan\"";
+            string newCulture = "ohio";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) =>
+            {
+                if (s == "culture")
+                    p.WriteValue(newCulture, quoteWrap: true);
+            };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            Assert.AreEqual("culture=\"ohio\"", save.ToString());
+        }
+
+        [Test]
+        public void WriteNumericalList()
+        {
+            List<int> list = new List<int>() { 1, 2, 3 };
+            string input = "numbers={ 3 2 1 }";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) =>
+            {
+                if (s == "numbers")
+                    p.WriteList(list, appendNewLine: false);
+            };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            Assert.AreEqual("numbers={ 1 2 3 }", save.ToString());
+        }
+
+        [Test]
+        public void WriteQuotedList()
+        {
+            List<string> list = new List<string>() { "Infi a", "Infi B" };
+            string input = "reg={ }";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) =>
+            {
+                if (s == "reg")
+                    p.WriteList(list, appendNewLine: false, quoteWrap: true);
+            };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            Assert.AreEqual("reg={ \"Infi a\" \"Infi B\" }", save.ToString());
+        }
+
+        [Test]
+        public void WriteTechnologyList()
+        {
+            List<string> list = new List<string>() { "infantry_theory", "militia_theory", "mobile_theory" };
+            string input = "theoretical={ }";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) =>
+            {
+                if (s == "theoretical")
+                    p.WriteList(list, appendNewLine: false, delimiter: Environment.NewLine);
+            };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            string expected = "theoretical={\r\n\tinfantry_theory\r\n\tmilitia_theory\r\n\tmobile_theory\r\n}";
+            Assert.AreEqual(expected, save.ToString());
+        }
+
+        [Test]
+        public void IgnoreList()
+        {
+            string input = "list={1 2 3 4} list2={1 2 3 4}";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) =>
+                {
+                    if (s == "list2")
+                        p.WriteList(new int[] { 4, 3, 2, 1 });
+                };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            string expected = "list={1 2 3 4} list2={ 4 3 2 1 }";
+            Assert.AreEqual(expected, save.ToString());
+        }
+        //[Test]
+        //public void tabbedSingleSave()
+        //{
+        //    string input = "\tculture=michigan";
+        //    string newCulture = "ohio";
+        //    StringWriter save = new StringWriter();
+
+        //    Action<ParadoxSaver, string> action = (p, s) =>
+        //    {
+        //        if (s == "culture")
+        //            p.WriteValue(newCulture);
+        //    };
+        //    ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+        //    Assert.AreEqual("\tculture=ohio", save.ToString()); 
+        //}
+        [Test]
+        public void WriteIgnored()
+        {
+            string input = "culture=michigan\r\ncity=me";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) => { };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            Assert.AreEqual(input + Environment.NewLine, save.ToString());
+        }
+        [Test]
+        public void writeTabbed()
+        {
+            string input = "advisor={\r\n\tid=1562\r\n\ttype=39\r\n}";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) => 
+            {
+                if (s == "id")
+                    p.WriteValue("1", appendNewLine: true);
+                else if (s == "type")
+                    p.WriteValue("2", appendNewLine: true);
+            };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            string expected = "advisor={\r\n\tid=1\r\n\ttype=2\r\n}";
+            Assert.AreEqual(expected, save.ToString());
+        }
+        [Test]
+        public void writeIgnoredTabbed()
+        {
+            string input = "advisor={\r\n\tid=1562\r\n\ttype=39\r\n}";
+            StringWriter save = new StringWriter();
+            Action<ParadoxSaver, string> action = (p, s) => {  };
+            ParadoxSaver t = new ParadoxSaver(save, input.ToByteArray(), action);
+            string expected = "advisor={\r\n\tid=1562\r\n\ttype=39\r\n}";
+            Assert.AreEqual(expected, save.ToString());
+        }
+    }
+}
---
+++ Pdoxcl2Sharp/ParadoxSaver.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Pdoxcl2Sharp
+{
+    public class ParadoxSaver
+    {
+        private enum WriteType
+        {
+            Single,
+            List,
+            None
+        }
+        private TextWriter writer;
+        private ParadoxParser underlyingParser;
+        private WriteType lastWrite;
+        private StringBuilder builder;
+        private int prevIndex;
+        private LexerToken prevToken;
+        public ParadoxSaver(TextWriter output, byte[] data, Action<ParadoxSaver, string> action)
+        {
+            if (output == null)
+                throw new ArgumentNullException("output", "Must provide an output to write data");
+
+            if (action == null)
+                throw new ArgumentNullException("action", "Must define an action when saving");
+
+            writer = output;
+            builder = new StringBuilder();
+            prevIndex = 0;
+            prevToken = LexerToken.Untyped;
+            Action<ParadoxParser, string> parser = (p, s) =>
+                {
+                    lastWrite = WriteType.None;
+                    underlyingParser = p;
+                    action(this, s);
+
+
+                    switch (lastWrite)
+                    {
+                        case WriteType.Single:
+                            p.ReadString();
+                            break;
+                        case WriteType.None:
+                            preprocess(p.CurrentToken);
+
+                            if (p.CurrentToken == LexerToken.Equals)
+                                writer.Write(new string('\t', underlyingParser.CurrentIndex));
+                            writer.Write(s);
+
+                            if (prevIndex > underlyingParser.CurrentIndex)
+                                writer.Write("} ");
+
+                            if (p.CurrentToken == LexerToken.Equals)
+                                writer.Write('=');
+                            else if (p.CurrentToken == LexerToken.Untyped && prevToken == LexerToken.Equals
+                                && prevIndex >= underlyingParser.CurrentIndex)
+                                writer.Write(writer.NewLine);
+                            else if (p.CurrentToken == LexerToken.Untyped)
+                                writer.Write(' ');
+                            break;
+                        case WriteType.List:
+                            p.ReadStringList();
+                            break;
+                    }
+                    prevIndex = underlyingParser.CurrentIndex;
+                    prevToken = underlyingParser.CurrentToken;
+                };
+            ParadoxParser.Parse(data, parser);
+            if (prevIndex != 0)
+                writer.Write('}');
+        }
+        private void preprocess(LexerToken token)
+        {
+            if (prevIndex < underlyingParser.CurrentIndex)
+            {
+                writer.Write('{');
+                if (token == LexerToken.Equals)
+                    writer.Write(writer.NewLine);
+            }
+        }
+        public void WriteValue(string value, bool appendNewLine = false, bool quoteWrap = false)
+        {
+            builder.Clear();
+            lastWrite = WriteType.Single;
+            preprocess(prevToken);
+
+            builder.Append('\t', underlyingParser.CurrentIndex);
+            builder.Append(underlyingParser.CurrentString);
+            builder.Append('=');
+            if (quoteWrap)
+            {
+                builder.Append('"');
+                builder.Append(value);
+                builder.Append('"');
+            }
+            else
+            {
+                builder.Append(value);
+            }
+            if (appendNewLine)
+                builder.Append(writer.NewLine);
+            writer.Write(builder.ToString());
+        }
+
+        public void WriteList<T>(IEnumerable<T> list, bool appendNewLine = false,
+            string delimiter = " ", bool quoteWrap = false)
+        {
+            builder.Clear();
+            lastWrite = WriteType.List;
+            preprocess(prevToken);
+
+
+            builder.Append('\t', underlyingParser.CurrentIndex);
+            builder.Append(underlyingParser.CurrentString);
+
+
+            if (delimiter.Contains('\r') || delimiter.Contains('\n'))
+            {
+                writeNewLineDelimitedList(list, appendNewLine: appendNewLine, quoteWrap: quoteWrap);
+                return;
+            }
+
+            builder.Append("={ ");
+
+            if (!quoteWrap)
+            {
+                foreach (var value in list)
+                {
+                    builder.Append(value.ToString());
+                    builder.Append(delimiter);
+                }
+            }
+            else
+            {
+                foreach (var value in list)
+                {
+                    builder.Append('"');
+                    builder.Append(value.ToString());
+                    builder.Append('"');
+                    builder.Append(delimiter);
+                }
+            }
+
+            builder = builder.Remove(builder.Length - delimiter.Length, delimiter.Length);
+
+            builder.Append(" }");
+            if (appendNewLine)
+                builder.Append(writer.NewLine);
+            writer.Write(builder.ToString());
+        }
+        private void writeNewLineDelimitedList<T>(IEnumerable<T> list, bool appendNewLine = false, bool quoteWrap = false)
+        {
+            builder.Append("={");
+            builder.Append(writer.NewLine);
+
+            if (!quoteWrap)
+            {
+                foreach (var value in list)
+                {
+                    builder.Append('\t', underlyingParser.CurrentIndex + 1);
+                    builder.Append(value.ToString());
+                    builder.Append(writer.NewLine);
+                }
+            }
+            else
+            {
+                foreach (var value in list)
+                {
+                    builder.Append('\t', underlyingParser.CurrentIndex + 1);
+                    builder.Append('"');
+                    builder.Append(value.ToString());
+                    builder.Append('"');
+                    builder.Append(writer.NewLine);
+                }
+            }
+            builder.Append('\t', underlyingParser.CurrentIndex);
+            builder.Append('}');
+            if (appendNewLine)
+                builder.Append(writer.NewLine);
+            writer.Write(builder.ToString());
+        }
+    }
+}
