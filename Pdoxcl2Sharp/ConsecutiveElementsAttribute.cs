036f9db57226a1bb5537aa270a202eab5f411f6b
Nick Babcock <nbabcock19@hotmail.com>
2014-06-23 12:27:35 -0500

    Add parsing lists to parse template

---
+++ Pdoxcl2Sharp/ConsecutiveElementsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pdoxcl2Sharp
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ConsecutiveElementsAttribute : Attribute
+    {
+    }
+}
---
+++ Pdoxcl2Sharp/InflectorExtensions.cs
@@ -0,0 +1,269 @@
+﻿// The Inflector class was cloned from Humanizer (https://github.com/MehdiK/Humanizer)
+// The Inflector class was cloned from Inflector (https://github.com/srkirkland/Inflector)
+// The MIT License (MIT)
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Pdoxcl2Sharp
+{
+    /// <summary>
+    /// Provides hint as to whether a word is singular, plural or with unknown plurality
+    /// </summary>
+    public enum Plurality
+    {
+        /// <summary>
+        /// The word is singular
+        /// </summary>
+        Singular,
+        /// <summary>
+        /// The word is plural
+        /// </summary>
+        Plural,
+        /// <summary>
+        /// I am unsure of the plurality
+        /// </summary>
+        CouldBeEither
+    }
+
+    /// <summary>
+    /// Inflector extensions
+    /// </summary>
+    public static class InflectorExtensions
+    {
+        static InflectorExtensions()
+        {
+            AddPlural("$", "s");
+            AddPlural("s$", "s");
+            AddPlural("(ax|test)is$", "$1es");
+            AddPlural("(octop|vir|alumn|fung)us$", "$1i");
+            AddPlural("(alias|status)$", "$1es");
+            AddPlural("(bu)s$", "$1ses");
+            AddPlural("(buffal|tomat|volcan)o$", "$1oes");
+            AddPlural("([ti])um$", "$1a");
+            AddPlural("sis$", "ses");
+            AddPlural("(?:([^f])fe|([lr])f)$", "$1$2ves");
+            AddPlural("(hive)$", "$1s");
+            AddPlural("([^aeiouy]|qu)y$", "$1ies");
+            AddPlural("(x|ch|ss|sh)$", "$1es");
+            AddPlural("(matr|vert|ind)ix|ex$", "$1ices");
+            AddPlural("([m|l])ouse$", "$1ice");
+            AddPlural("^(ox)$", "$1en");
+            AddPlural("(quiz)$", "$1zes");
+
+            AddSingular("s$", "");
+            AddSingular("(n)ews$", "$1ews");
+            AddSingular("([ti])a$", "$1um");
+            AddSingular("((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$", "$1$2sis");
+            AddSingular("(^analy)ses$", "$1sis");
+            AddSingular("([^f])ves$", "$1fe");
+            AddSingular("(hive)s$", "$1");
+            AddSingular("(tive)s$", "$1");
+            AddSingular("([lr])ves$", "$1f");
+            AddSingular("([^aeiouy]|qu)ies$", "$1y");
+            AddSingular("(s)eries$", "$1eries");
+            AddSingular("(m)ovies$", "$1ovie");
+            AddSingular("(x|ch|ss|sh)es$", "$1");
+            AddSingular("([m|l])ice$", "$1ouse");
+            AddSingular("(bus)es$", "$1");
+            AddSingular("(o)es$", "$1");
+            AddSingular("(shoe)s$", "$1");
+            AddSingular("(cris|ax|test)es$", "$1is");
+            AddSingular("(octop|vir|alumn|fung)i$", "$1us");
+            AddSingular("(alias|status)es$", "$1");
+            AddSingular("^(ox)en", "$1");
+            AddSingular("(vert|ind)ices$", "$1ex");
+            AddSingular("(matr)ices$", "$1ix");
+            AddSingular("(quiz)zes$", "$1");
+
+            AddIrregular("person", "people");
+            AddIrregular("man", "men");
+            AddIrregular("child", "children");
+            AddIrregular("sex", "sexes");
+            AddIrregular("move", "moves");
+            AddIrregular("goose", "geese");
+            AddIrregular("alumna", "alumnae");
+
+            AddUncountable("equipment");
+            AddUncountable("information");
+            AddUncountable("rice");
+            AddUncountable("money");
+            AddUncountable("species");
+            AddUncountable("series");
+            AddUncountable("fish");
+            AddUncountable("sheep");
+            AddUncountable("deer");
+            AddUncountable("aircraft");
+        }
+
+        private class Rule
+        {
+            private readonly Regex _regex;
+            private readonly string _replacement;
+
+            public Rule(string pattern, string replacement)
+            {
+                _regex = new Regex(pattern, RegexOptions.IgnoreCase);
+                _replacement = replacement;
+            }
+
+            public string Apply(string word)
+            {
+                if (!_regex.IsMatch(word))
+                {
+                    return null;
+                }
+
+                return _regex.Replace(word, _replacement);
+            }
+        }
+
+        private static void AddIrregular(string singular, string plural)
+        {
+            AddPlural("(" + singular[0] + ")" + singular.Substring(1) + "$", "$1" + plural.Substring(1));
+            AddSingular("(" + plural[0] + ")" + plural.Substring(1) + "$", "$1" + singular.Substring(1));
+        }
+
+        private static void AddUncountable(string word)
+        {
+            Uncountables.Add(word.ToLower());
+        }
+
+        private static void AddPlural(string rule, string replacement)
+        {
+            Plurals.Add(new Rule(rule, replacement));
+        }
+
+        private static void AddSingular(string rule, string replacement)
+        {
+            Singulars.Add(new Rule(rule, replacement));
+        }
+
+        private static readonly List<Rule> Plurals = new List<Rule>();
+        private static readonly List<Rule> Singulars = new List<Rule>();
+        private static readonly List<string> Uncountables = new List<string>();
+
+        /// <summary>
+        /// Pluralizes the provided input considering irregular words
+        /// </summary>
+        /// <param name="word">Word to be pluralized</param>
+        /// <param name="plurality">Normally you call Pluralize on singular words; but if you're unsure call it with Plurality.CouldBeEither</param>
+        /// <returns></returns>
+        public static string Pluralize(this string word, Plurality plurality = Plurality.Singular)
+        {
+            if (plurality == Plurality.Plural)
+                return word;
+
+            var result = ApplyRules(Plurals, word);
+
+            if (plurality == Plurality.Singular)
+                return result;
+
+            var asSingular = ApplyRules(Singulars, word);
+            var asSingularAsPlural = ApplyRules(Plurals, asSingular);
+            if (asSingular != null && asSingular != word && asSingular + "s" != word && asSingularAsPlural == word && result != word)
+                return word;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Singularizes the provided input considering irregular words
+        /// </summary>
+        /// <param name="word">Word to be singularized</param>
+        /// <param name="plurality">Normally you call Singularize on plural words; but if you're unsure call it with Plurality.CouldBeEither</param>
+        /// <returns></returns>
+        public static string Singularize(this string word, Plurality plurality = Plurality.Plural)
+        {
+            if (plurality == Plurality.Singular)
+                return word;
+
+            var result = ApplyRules(Singulars, word);
+
+            if (plurality == Plurality.Plural)
+                return result;
+
+            // the Plurality is unknown so we should check all possibilities
+            var asPlural = ApplyRules(Plurals, word);
+            var asPluralAsSingular = ApplyRules(Singulars, asPlural);
+            if (asPlural != word && word + "s" != asPlural && asPluralAsSingular == word && result != word)
+                return word;
+
+            return result ?? word;
+        }
+
+        private static string ApplyRules(List<Rule> rules, string word)
+        {
+            if (word == null)
+                return null;
+
+            var result = word;
+
+            if (!Uncountables.Contains(word.ToLower()))
+            {
+                for (int i = rules.Count - 1; i >= 0; i--)
+                {
+                    if ((result = rules[i].Apply(word)) != null)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// By default, pascalize converts strings to UpperCamelCase also removing underscores
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Pascalize(this string input)
+        {
+            return Regex.Replace(input, "(?:^|_)(.)", match => match.Groups[1].Value.ToUpper());
+        }
+
+        /// <summary>
+        /// Same as Pascalize except that the first character is lower case
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Camelize(this string input)
+        {
+            string word = Pascalize(input);
+            return word.Substring(0, 1).ToLower() + word.Substring(1);
+        }
+
+        /// <summary>
+        /// Separates the input words with underscore
+        /// </summary>
+        /// <param name="input">The string to be underscored</param>
+        /// <returns></returns>
+        public static string Underscore(this string input)
+        {
+            return Regex.Replace(
+                Regex.Replace(
+                    Regex.Replace(input, @"([A-Z]+)([A-Z][a-z])", "$1_$2"), @"([a-z\d])([A-Z])", "$1_$2"), @"[-\s]", "_").ToLower();
+        }
+
+        /// <summary>
+        /// Replaces underscores with dashes in the string
+        /// </summary>
+        /// <param name="underscoredWord"></param>
+        /// <returns></returns>
+        public static string Dasherize(this string underscoredWord)
+        {
+            return underscoredWord.Replace('_', '-');
+        }
+
+        /// <summary>
+        /// Replaces underscores with hyphens in the string
+        /// </summary>
+        /// <param name="underscoredWord"></param>
+        /// <returns></returns>
+        public static string Hyphenate(this string underscoredWord)
+        {
+            return Dasherize(underscoredWord);
+        }
+    }
+}
