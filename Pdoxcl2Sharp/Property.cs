7c6e145ad5527efca1dfb97b4d945d0de2dc7b4f
Nick Babcock <nbabcock19@hotmail.com>
2014-06-24 22:11:37 -0500

    Define property class

---
+++ Pdoxcl2Sharp/Property.cs
@@ -0,0 +1,36 @@
+﻿namespace Pdoxcl2Sharp
+{
+    public class Property
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public bool IsNonConsecutiveList
+        {
+            get
+            {
+                return (this.Type.Contains("ICollection<") ||
+                    this.Type.Contains("IList<") ||
+                    this.Type.Contains("List<")) &&
+                    !this.Type.Contains("[ConsecutiveElements]");
+            }
+        }
+
+        public string GetStr(INamingConvention naming)
+        {
+            if (!string.IsNullOrEmpty(this.Alias))
+                return this.Alias;
+            else if (this.IsNonConsecutiveList)
+                return naming.Apply(this.Name).Singularize(Plurality.CouldBeEither);
+            else
+                return naming.Apply(this.Name);
+        }
+
+        public string ExtractInnerListType()
+        {
+            var str = this.Type;
+            str = str.Substring(str.IndexOf('<') + 1);
+            return str.Remove(str.LastIndexOf('>'));
+        }
+    }
+}
